f9cc232624e6537f72b9915e6a6fb137e7e2e5e3
bbabatop@gmail.com <Jbaba.bbabatop@gmail.com>
2024-04-21 22:22:44 -0400

    new commit

---
+++ Triggaman/src/main/java/com/Triggaman/Triggaman/App.java
@@ -0,0 +1,13 @@
+package com.Triggaman.Triggaman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
